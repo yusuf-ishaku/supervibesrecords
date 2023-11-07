14fb729ec74edf0619f5db6880d09d79b5e89554
yusuf-ishaku <kingshakzy@gmail.com>
2023-11-07 20:34:16 +0100

    Fixed error

--- app/data/store.js
+++ app/data/store.js
@@ -1,8 +1,8 @@
 "use client"
 
 import { configureStore } from "@reduxjs/toolkit";
-import { generalApiSlice } from "./apiSlice/generalApiSlice";
-import userReducer from "./dataSlice/userSlice"
+import { generalApiSlice } from "./api/generalApiSlice";
+// import userReducer from "./dataSlice/userSlice"
 
 export const store = configureStore({
     reducer: {
