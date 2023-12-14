c959512738319ad90ef674074a5e57e827809728
yusuf-ishaku <kingshakzy@gmail.com>
2023-12-14 23:23:09 +0100

    Fixed playing sound issues

--- app/discography/audio/[audio]/page.js
+++ app/discography/audio/[audio]/page.js
@@ -9,11 +9,8 @@ export default function Page({params}) {
     let {data: audio, isFetching, isLoading} = useGetAudioQuery(params.audio);
     console.log(audio)
   return (
-    <section className="w-[100vw] h-[100vh] bg-[#0A0B14] flex flex-col items-center ">
-       {
-      audio?.data.code === 200 ?  
- ( <> 
-<div className="bg-gray-600 w-[80%] h-fit rounded-md mt-28">
+    <section className="w-[100vw] h-[100vh] bg-[#0A0B14] flex flex-col items-center "> 
+      <div className="bg-gray-600 w-[80%] h-fit rounded-md mt-28">
              <img className="rounded-md" src={audio?.data.imageUrl}>
             </img>
         </div>
@@ -25,27 +22,7 @@ export default function Page({params}) {
             { isLoading ?  "Singer" : audio?.data.artiste }
         </h3>
         </header>
-       <MusicNowPlaying artiste={audio?.data.artiste} title={audio?.data.title} audio = {isLoading ? null : audio?.data.audioUrl} text={`https://supervibesrecords.com/discography/audio/${params.audio}`}></MusicNowPlaying>
-</>
-)
-:
- (<>
-<div className="bg-gray-600 w-[80%] h-fit rounded-md mt-28">
-             <img className="rounded-md" src={audio?.data.imageUrl}>
-            </img>
-        </div>
-        <header className="mt-4 flex flex-col items-center">
-        <h2 className="text-[#fa0] text-2xl">
-            { isLoading ? "Song Title" : audio?.data.title}
-        </h2>
-        <h3 className="text-white text-sm">
-            { isLoading ?  "Singer" : audio?.data.artiste }
-        </h3>
-        </header>
-       <MusicNowPlaying audio = {isLoading ? null : audio?.data.audioUrl} text={`https://supervibesrecords.com/discography/audio/${params.audio}`}></MusicNowPlaying>
-</>
-)
-}
+    {audio?.data && <MusicNowPlaying artiste={audio?.data.artiste} title={audio?.data.title} audio = {isLoading ? null : audio?.data.audioUrl} text={`https://supervibesrecords.com/discography/audio/${params.audio}`}></MusicNowPlaying>} 
     </section>
   )
 }
